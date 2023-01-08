3740badf00866a934c1b8af6c79836d85546b3bb
bonov68 <example@mail.ru>
2023-01-08 13:52:11 +0300

    found count of symbol of each item

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿// Написать программу, которая из имеющегося массива строк, формирует массив строк,длина кторых меньше либо равна 3 символа.
 
-["pictures", "2", "river", "+&*" ];
-["1234", "567", "new year", "-1" ];
-["Tomato", "Football", "Kitchen"];
+// ["pictures", "2", "river", "+&*" ];
+// ["1234", "567", "new year", "-1" ];
+// ["Tomato", "Football", "Kitchen"];
 
+//
+string [] arr = {"pictures", "2", "river", "+&*"};
 
+string [] sortedArr = {}; 
+int count = 0; 
+
+for ( int i = 0; i < arr.Length; i++)
+{
+ Console.WriteLine(arr[i].Length);
+}
+
+// string str = "pictures";
+// int lengh = str.Length;
+
+// Console.WriteLine("The length of '{0}' is {1}", str, str.Length);
+// Console.WriteLine("The length of '{0}' is {1}", "+&*", "+&*".Length);
