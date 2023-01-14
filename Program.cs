5588367255421fbb76198a7ce43d7474fad3dd77
bonov68 <example@mail.ru>
2023-01-14 13:14:34 +0300

    removed unnecessary strings

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
 ﻿// Написать программу, которая из имеющегося массива строк, формирует массив строк,длина
 //  которых меньше либо равна 3 символа.
 
-// ["pictures", "2", "river", "+&*", "cat"];
-// ["1234", "567", "new year", "-1" ];
-// ["Tomato", "Football", "Kitchen"];
-
-// Console.WriteLine(arr[i].Length);
-//
 // string[] arr = { "pictures", "2", "river", "+&*", "cat"};
-string[] arr = { "1234", "567", "new year", "-1" };
+// string[] arr = { "1234", "567", "new year", "-1" };
+string[] arr = { "Tomato", "Football", "Kitchen" };
+
 string[] sortedArr = new string[arr.Length];
 
 Console.Write("[");
@@ -16,17 +12,14 @@ for (int i = 0; i < arr.Length; i++)
 {
     if (arr[i].Length <= 3)
     {
-
         sortedArr[i] = arr[i] ;
         Console.Write($"{sortedArr[i]},");
     }
-  
 }
 Console.Write("]"); 
 Console.WriteLine();
 
 sortedArr = sortedArr.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
-// sortedArr = sortedArr.Where(a => "" != a).ToArray();
 
 for (int i = 0; i < sortedArr.Length; i++)
 {
